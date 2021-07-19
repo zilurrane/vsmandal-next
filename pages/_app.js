b39e8d07e0efb8bc000461ed0063efc4b7f72d24
zilurrane <zilurrane@gmail.com>
2021-07-19 22:46:02 +0530

    Added NextSEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import { NextSeo } from 'next-seo';
 import '../styles/globals.scss'
 import 'react-modal-video/scss/modal-video.scss';
 import '../public/css/font-awesome.min.css';
@@ -5,7 +6,14 @@ import '../public/css/themify-icons.css';
 import '../public/css/flaticon.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <>
+    <NextSeo
+      title="Vivekanand Seva Mandal"
+      description="Vivekanand Seva Mandal is basically a group of young philanthropists involving themselves in a Rural Development Programs with a view of social development while pursuing their own academic/ professional activities."
+    />
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
