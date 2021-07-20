eb7648ff12e8e2df04b15feb4e3757bc4f5cc46d
zilurrane <zilurrane@gmail.com>
2021-07-20 22:22:41 +0530

    Fix left right scroll icon issue

--- components/case/index.js
+++ components/case/index.js
@@ -1,8 +1,6 @@
 
 import React, { Component } from 'react'
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import Link from 'next/link'
 import Image from 'next/image'
 
