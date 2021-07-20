c3c757abdf2ff27c68e4d3e282408f1e27696024
zilurrane <zilurrane@gmail.com>
2021-07-20 17:52:26 +0530

    Added Projects Page

---
+++ pages/projects/index.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+import Header from '../../components/header';
+import Footer from '../../components/footer';
+import PageTitle from '../../components/pagetitle'
+
+const Projects = () => {
+    return (
+        <Fragment>
+            <Header />
+            <PageTitle pageTitle={'Project Management'} pagesub={'Handle Projects'} />
+            <div className="wpo-donation-page-area section-padding">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-8 offset-lg-2">
+                            <div className="wpo-donate-header">
+                                <h2>Projects</h2>
+                            </div>
+                            show add new project button
+                            <br />
+
+                            show table below as:<br />
+                            project name     description     other deatils(if needed)    edit and delete button for each project
+                            {/* <Table /> with column names to be passed , which  buttons to show that to be passed */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </Fragment>
+    )
+};
+
+export default Projects;
