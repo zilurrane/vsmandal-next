85bbacb4f8465a10a4d9cc3db0a11eaee7fd8452
VAJRESH <vajresh005@gmail.com>
2023-01-20 13:36:08 +0530

    fix: data type updated for api response

--- shared/api.js
+++ shared/api.js
@@ -12,39 +12,45 @@ export const getHeaderMenuJson = async () => {
 };
 
 export const getAllProjects = async () => {
-  const res = await fetch(`${API_BASE_URL}/projects`);
-  const data = await res.json();
-  return data;
+  return []
+  // const res = await fetch(`${API_BASE_URL}/projects`);
+  // const data = await res.json();
+  // return data;
 };
 
 export const getProjectById = async (id) => {
-  const res = await fetch(`${API_BASE_URL}/projects/${id}`);
-  const data = await res.json();
-  return data;
+  return {}
+  // const res = await fetch(`${API_BASE_URL}/projects/${id}`);
+  // const data = await res.json();
+  // return data;
 };
 
 export const getProjectByUrlSlug = async (slug) => {
-  const res = await fetch(`${API_BASE_URL}/projects/slug/${slug}`);
-  const data = await res.json();
-  return data;
+  return {}
+  // const res = await fetch(`${API_BASE_URL}/projects/slug/${slug}`);
+  // const data = await res.json();
+  // return data;
 };
 
 export const getAllEvents = async () => {
-  const res = await fetch(`${API_BASE_URL}/events`);
-  const data = await res.json();
-  return data;
+  return []
+  // const res = await fetch(`${API_BASE_URL}/events`);
+  // const data = await res.json();
+  // return data;
 };
 
 export const getEventByUrlSlug = async (slug) => {
-  const res = await fetch(`${API_BASE_URL}/events/slug/${slug}`);
-  const data = await res.json();
-  return data;
+  return {}
+  // const res = await fetch(`${API_BASE_URL}/events/slug/${slug}`);
+  // const data = await res.json();
+  // return data;
 };
 
 export const getEventById = async (id) => {
-  const res = await fetch(`${API_BASE_URL}/events/${id}`);
-  const data = await res.json();
-  return data;
+  return {}
+  // const res = await fetch(`${API_BASE_URL}/events/${id}`);
+  // const data = await res.json();
+  // return data;
 };
 
 
